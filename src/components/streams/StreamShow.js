d65457c0f2f87f2fb84334b3ea251aa37db13a1f
Aamir <aamir.nu.206@gmail.com>
2019-01-29 23:23:47 +0500

    CRUD operation on streams project.

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchStream} from "../../actions";
 
-const StreamShow = () => {
-    return (
-        <div>StreamShow</div>
-    )
+class StreamShow extends React.Component{
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id)
+    }
+
+    render() {
+        const {title, description} = this.props.stream;
+
+        if(!this.props.stream){
+            return <div>loading...</div>
+        }
+        return (
+            <div className="content">
+                <h3>{title}</h3>
+                <h5>{description}</h5>
+            </div>
+        );
+    }
+}
+
+const mapStateToProp = (state, ownProps) => {
+    return {stream: state.streams[ownProps.match.params.id]}
 };
 
-export default StreamShow;
+export default connect(mapStateToProp, {fetchStream})(StreamShow);
